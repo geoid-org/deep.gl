a024058b270ae8e53c2c8fafcb42678b0ac28622
Lars van Vianen <lars@vianen.com>
2023-07-28 13:17:23 +0200

    Create mean_interquartile.ts

---
+++ src/statistics/src/location/mean_interquartile.ts
@@ -0,0 +1,35 @@
+// Copyright 2023 Geoid
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+ * Interquartile Mean
+ * @static
+ * @memberof
+ * @class
+ * @description
+ * @param data data set
+ * @returns {number}
+ * @example
+ * @see {@link https://en.wikipedia.org/wiki/Interquartile_mean Wikipedia: Interquartile Mean}
+ */
+ export const interquartilecMean = (
+
+    data: Array<number>,
+
+): number => {
+
+    
+
+}
