399fd17dee379415ad5a1108d049f9e046583f40
Lars van Vianen <lars@vianen.com>
2023-07-28 13:17:15 +0200

    Create midrange.ts

---
+++ src/statistics/src/location/midrange.ts
@@ -0,0 +1,34 @@
+// Copyright 2023 Geoid
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+ * Midrange
+ * @static
+ * @memberof
+ * @class
+ * @description 
+ * @param data data set
+ * @returns {number}
+ * @example
+ * @see {@link https://en.wikipedia.org/wiki/Midrange Wikipedia: Midrange}
+ */
+ export const quantile = (
+
+    data: Array<number>,
+  
+): number => {
+
+
+};
